25d8c1a9a095f6c1e30c29ac64e4eda17b14fe39
Antoine Van Uffelen <antoine@mansio.be>
2024-04-03 13:21:59 +0200

    Added extraction of links & images

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -5,6 +5,7 @@ from textnode import (
     text_type_italic,
     text_type_code,
 )
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -26,3 +27,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
     
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)",text)
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)",text)
